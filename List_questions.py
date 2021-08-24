6f37ecdc9d31edc5877325e6f5bc3582557ad51d
acadroit <73344265+acadroit@users.noreply.github.com>
2021-08-24 15:59:50 +0530

    Update List_questions.py

--- List_questions.py
+++ List_questions.py
@@ -9,7 +9,7 @@ def minimum(lst):
             min=a
     return min
 print(minimum([1,3,44,"abhi",555667,72,100]))
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Write a Python program to remove duplicates from a list
 lst=['abc', 'xyz', 'aba', '1221',22,44,55,22,'abc']
 
@@ -17,7 +17,7 @@ for value in lst:
 	if lst.count(value)>1:
 		lst.remove(value)
 print(lst)
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
 lst=['abc', 'xyz', 'aba', '1221']
 
@@ -28,7 +28,7 @@ def same(lst):
 			new_lst.append(n)	
 	print(new_lst)
 same(lst)
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Write a python program to count character occurrences: 
 l=[1,2,3,1,2,2,5,6]
@@ -41,7 +41,7 @@ def count(lst):
         repeat.update({b:a})
     return repeat
 print(count(l))
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #sorting a list with sorted function 
 sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
@@ -50,3 +50,34 @@ def second_element(elem):
 
 sort=sorted(sample_list, key=second_element)
 print(sort)
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Q. Write a Python program to count the number of elements in a list within a specified range
+#Using Function
+
+def comp(li,min,max):
+    ctr=0
+    for value in li:
+        if min<=value<=max:
+            ctr+=1
+    return ctr
+list2 = ['a','b','c','d','e','f','e','e','e']
+print(comp(list2,"a","e"))
+
+# Same question solved using OOP
+
+class Comp:
+    def __init__(self,li,min,max):
+        self.li=li
+        self.min=min
+        self.max=max
+    def compare(self):
+        ctr=0
+        for value in self.li:
+            if self.min<=value<=self.max:
+                ctr+=1
+        return ctr
+list2 = ['a','b','c','d','e','f','e','e','e']
+obj1=Comp(list2,"a","e")
+print(obj1.compare())
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
